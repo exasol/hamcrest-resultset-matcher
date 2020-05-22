e236e6dc489299f0acdd94d720b8b767ceb091f9
Sebastian Bär <sebastian.baer@exasol.com>
2020-05-22 10:22:49 +0200

    #4: Added missing JavaDoc.

--- src/main/java/com/exasol/matcher/Column.java
+++ src/main/java/com/exasol/matcher/Column.java
@@ -14,22 +14,49 @@ public class Column implements SelfDescribing {
         this.typeName = typeName;
     }
 
-    static Column any() {
+    /**
+     * Create a new instance of a column description where the type is not validated.
+     *
+     * @return new instance
+     */
+    public static Column any() {
         return new Column(ANY_TYPE);
     }
 
-    static Column column(final String typeName) {
+    /**
+     * Create a new instance of a column description where the type is not validated.
+     *
+     * @param typeName name of the type that is checked against the column's type
+     *
+     * @return new instance
+     */
+    public static Column column(final String typeName) {
         return new Column(typeName);
     }
 
+    /**
+     * Get the name of the column's data type.
+     *
+     * @return data type name
+     */
     public String getTypeName() {
         return this.typeName;
     }
 
+    /**
+     * Check whether a data type is specified.
+     *
+     * @return {@code true} if a data type is given
+     */
     public boolean hasType() {
         return !ANY_TYPE.equals(this.typeName);
     }
 
+    /**
+     * Check if any validation criteria are specified.
+     * 
+     * @return {@code true} if any validation criteria are specified.
+     */
     public boolean isSpecified() {
         return hasType();
     }
