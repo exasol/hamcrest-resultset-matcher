d3f35538948de519e2a81041597415f8e78a2bbf
Sebastian Bär <sebastian.baer@exasol.com>
2023-10-27 14:00:45 +0200

    Fixed lambda code smells.

--- src/test/java/com/exasol/matcher/RowMatcherIT.java
+++ src/test/java/com/exasol/matcher/RowMatcherIT.java
@@ -1,11 +1,13 @@
 package com.exasol.matcher;
 
+import org.hamcrest.Matcher;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -32,12 +34,13 @@ class RowMatcherIT extends AbstractResultSetMatcherTest {
     void testMatchingInAnyOrderAndExpectingThreeColumnsThrowsAssertionErrorWhenResultSetHasOnlyTwoColumns() {
         execute("CREATE TABLE T(I INTEGER, V VARCHAR(20))");
         execute("INSERT INTO T VALUES (1, 'a'), (2, 'b')");
-        AssertionError error = assertThrows(AssertionError.class, () -> ResultSetStructureMatcher //
+        final Matcher<ResultSet> anyOrderMatcher = ResultSetStructureMatcher //
                 .table()//
                 .row(1, "a", 1) //
                 .row(greaterThan(0), "b", 3) //
-                .matchesInAnyOrder() //
-                .matches(query("SELECT * FROM T")));
+                .matchesInAnyOrder();
+        AssertionError error = assertThrows(AssertionError.class,
+                () -> anyOrderMatcher.matches(query("SELECT * FROM T")));
         assertThat(error.getMessage(), startsWith("Row expectation definition 1 tries to validate the value of row 1, "
                 + "column 3 but that value can't be read from the result set. "
                 + "This usually means the column does not exist. \nCaused by SQL error:"));
@@ -48,12 +51,13 @@ class RowMatcherIT extends AbstractResultSetMatcherTest {
     void testMatchingInStrictOrderAndExpectingThreeColumnsThrowsAssertionErrorWhenResultSetHasOnlyTwoColumns() {
         execute("CREATE TABLE T(I INTEGER, V VARCHAR(20))");
         execute("INSERT INTO T VALUES (1, 'A'), (2, 'B')");
-        AssertionError error = assertThrows(AssertionError.class, () -> ResultSetStructureMatcher //
+        final Matcher<ResultSet> orderedMatcher = ResultSetStructureMatcher //
                 .table()//
                 .row(1, "A", 1) //
                 .row(greaterThan(0), "B", 3) //
-                .matches() //
-                .matches(query("SELECT * FROM T")));
+                .matches();
+        AssertionError error = assertThrows(AssertionError.class,
+                () -> orderedMatcher.matches(query("SELECT * FROM T")));
         assertThat(error.getMessage(), startsWith("Row expectation definition 1 tries to validate the value of row 1, "
                 + "column 3 but that value can't be read from the result set. "
                 + "This usually means the column does not exist. \nCaused by SQL error:"));
